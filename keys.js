dbea2f291b283a06966eea56326aca05c46698a5
Rissaree <51396335+mcarrillo916@users.noreply.github.com>
2019-08-22 13:07:33 -0700

    liti js commands

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-console.log('this is loaded');
+require("dotenv").config();
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_ID
+};
+
+exports.BANDSINTOWN = {
+  api_key: process.env.BANDSINTOWN_ID
 };
--- liri.js
+++ liri.js
@@ -1,7 +1,31 @@
-var keys = require("./keys.js");
-var fs = require("fs");
-var spotify = new Spotify(keys.spotify);
-
 require("dotenv").config();
+var keys = require("./keys.js");
+var search = require("./search.js")
+var search = new search()
+var spotify = new spotify(keys.spotify);
+var fs = require("fs");
 
-"`concert-this`, `spotify-this-song`, `movie-this`, `do-what-it-says`"
+
+let input = process.argv[2]
+let question = process.argv.slice(3).join(".")
+
+function userCommand(input, question){
+    switch(input){
+        case `concert-this`:
+            concertThis();
+            break;
+        case `spotify-this-song`:
+            spotifyThisSong();
+            break;
+        case `movie-this`:
+            movieThis();
+            break;
+        case `do-what-it-says`:
+            doWhatItSays();
+            break;
+        default:
+            console.log("Say Whaaaaaaa?");
+            break;
+    }
+}
+userCommand(input, question)
